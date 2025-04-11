051f28f810dd82468204021625126ce36ca403e2
Abhay-989 <abhaychouhan8799@gmail.com>
2025-04-12 00:43:07 +0530

    commited

--- .js
+++ .js
@@ -111,14 +111,13 @@ searchBtn.addEventListener("click", function () {
       const leftsongname = document.querySelector('.left-song-name');
       const playPauseBtn = document.getElementById('playPauseBtn');
 
-     
 
       // main player source
       mainAudio.src = audio.src;
       mainAudio.play();
       currentIndex = index;
 
-      // Update UI
+    
       nowPlayingImg.src = imgSrc;
     
       footerPlayerImg.src = imgSrc;
@@ -137,3 +136,129 @@ searchBtn.addEventListener("click", function () {
   }
 });
 
+
+//  song list 
+const songs = [
+  {
+    id: 0,
+    name: "APT.",
+    audioSrc: "audio/apt.mp3",
+    imgSrc: "media/apt.png"
+  },
+  {
+    id: 1,
+    name: "Die With The Smile",
+    audioSrc: "audio/die-with-the-smile.mp3",
+    imgSrc: "media/die-with-the-smile.png"
+  },
+  {
+    id: 2,
+    name: "Unstopable",
+    audioSrc: "audio/unstopable.mp3",
+    imgSrc: "media/unstopable.png"
+  },
+  {
+    id: 3,
+    name: "Me And The Devil",
+    audioSrc: "audio/me-and-the-devil.mp3",
+    imgSrc: "media/me-and-the-devil.png"
+  },
+  {
+    id: 4,
+    name: "Jaana Samjho Na",
+    audioSrc: "audio/jaana-samjho-na.mp3",
+    imgSrc: "media/jaana-samjho-na.png"
+  },
+  {
+    id: 5,
+    name: "Ahista Ahista",
+    audioSrc: "audio/ahista-ahista.mp3",
+    imgSrc: "media/ahista-ahista.png"
+  },
+  {
+    id: 6,
+    name: "Uyi Amma",
+    audioSrc: "audio/uyi-amma.mp3",
+    imgSrc: "media/uyi-amma.png"
+  },
+  {
+    id: 7,
+    name: "Tu Hai kahan",
+    audioSrc: "audio/tu-hai-kahan.mp3",
+    imgSrc: "media/tu-hai-kahan.png"
+  },
+  {
+    id: 8,
+    name: "Skyfall",
+    audioSrc: "audio/skyfall.mp3",
+    imgSrc: "media/skyfall.png"
+  },
+  {
+    id: 9,
+    name: "Takki Takki",
+    audioSrc: "audio/takki-takki.mp3",
+    imgSrc: "media/takki-takki.png"
+  },
+  {
+    id: 10,
+    name: "Big Dawgs",
+    audioSrc: "audio/big-dawgs.mp3",
+    imgSrc: "media/big-dawgs.png"
+  },
+  {
+    id: 11,
+    name: "Espresso",
+    audioSrc: "audio/espresso.mp3",
+    imgSrc: "media/espresso.png"
+  },
+  {
+    id: 12,
+    name: "PASSO BEM SOLTO - Slowed - ATLXS",
+    audioSrc: "audio/PASSO BEM SOLTO - Slowed - ATLXS.mp3",
+    imgSrc: "media/PASSO BEM SOLTO - Slowed - ATLXS.png"
+  },
+  {
+    id: 13,
+    name: "MASHA ULTRAFUNK - HISTED",
+    audioSrc: "audio/MASHA ULTRAFUNK - HISTED.mp3",
+    imgSrc: "media/MASHA ULTRAFUNK - HISTED.png"
+  },
+  {
+    id: 14,
+    name: "FUNK DO BOUNCE (Slowed) - Ariis",
+    audioSrc: "audio/FUNK DO BOUNCE (Slowed) - Ariis.mp3",
+    imgSrc: "media/Funk de Beleza - Slowed - Nateki.png"
+  },
+  {
+    id: 15,
+    name: "BRODYAGA FUNK - Eternxlkz",
+    audioSrc: "audio/BRODYAGA FUNK - Eternxlkz.mp3",
+    imgSrc: "media/BRODYAGA FUNK - Eternxlkz.png"
+  }
+];
+
+
+const searchinput = document.getElementById("search-input");
+const searchResults = document.getElementById("searchResults");
+
+searchinput.addEventListener("input", () => {
+  const query = searchinput.value.toLowerCase();
+  searchResults.innerHTML = "";
+
+  if (query.trim() === "") return;
+
+  const filteredSongs = songs.filter(song =>
+    song.name.toLowerCase().includes(query)
+  );
+
+  filteredSongs.forEach(song => {
+    const li = document.createElement("li");
+    li.textContent = song.name;
+    li.addEventListener("click", () => {
+      searchinput.value = ""; 
+      searchResults.innerHTML = ""; 
+      toggleSong(song.id, song.imgSrc, song.name, song.audioSrc); 
+    });
+    searchResults.appendChild(li);
+  });
+});
